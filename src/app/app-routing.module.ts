a63b00f83185574865eb340815f4ee576eb670ce
Gustavo Galli <gustavosgalli@gmail.com>
2022-01-20 22:53:24 -0300

    @gustavogalli [Task: Inserindo rota de 'Cadastrar']

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 
   {path:'', redirectTo:'login', pathMatch:'full'},
 
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'cadastrar', component:CadastrarComponent}
+  
 ];
 
 @NgModule({
