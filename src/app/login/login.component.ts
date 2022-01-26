e7f15235399194630a2db27ac3582f105c72258d
João Almeida <78218571+joao295@users.noreply.github.com>
2022-01-26 15:31:55 -0300

    joao295 [Task : Add UsuarioLogin on Login C]

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioLogin } from '../model/UsuarioLogin';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  
+  usuarioLogin: UsuarioLogin = new UsuarioLogin
+  
   constructor() { }
 
+ 
+ 
+ 
+ 
+ 
   ngOnInit(): void {
   }
 
