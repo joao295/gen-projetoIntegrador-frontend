ebd091ef9b1a6c497dad63298724855243302837
CaioGabrielAlves <caio0401.ca@gmail.com>
2022-01-26 15:02:28 -0300

    @CaioGabrielAlves [Task:Adicionando Imports]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,15 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
-@Component({
-  selector: 'app-cadastrar',
-  templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css']
-})
-export class CadastrarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
