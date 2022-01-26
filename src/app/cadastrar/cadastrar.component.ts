2e24e11cacd733638b5e993df1d7cf76682174aa
CaioGabrielAlves <caio0401.ca@gmail.com>
2022-01-26 15:03:57 -0300

    @CaioGabrielAlves [Task:configurando constuctor]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -3,3 +3,18 @@ import { Router } from '@angular/router';
 import { Usuario } from '../model/Usuario';
 import { AuthService } from '../service/auth.service';
 
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css']
+})
+export class CadastrarComponent implements OnInit {
+
+  usuario:Usuario = new Usuario
+  confirmarSenha: string
+  tipoUsuario:string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
